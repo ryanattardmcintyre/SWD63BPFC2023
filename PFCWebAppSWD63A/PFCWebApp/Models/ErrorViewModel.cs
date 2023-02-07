e34f720852fc706a9b03d136cfeebee9eb4c9cbb
Ryan Attard <ryanattard@gmail.com>
2023-02-07 10:25:30 +0100

    Authentication

---
+++ PFCWebAppSWD63A/PFCWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PFCWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
